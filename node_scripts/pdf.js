8ebbb200756e60ba295f9d40aa9e0a2c5afead74
Josip Klaric <jklari@ekobit.hr>
2017-10-23 16:50:36 +0200

    Added template for generating pdf

---
+++ node_scripts/pdf.js
@@ -0,0 +1,10 @@
+﻿var htmlPdf = require('html-pdf');
+
+module.exports = function (result, options, data) {
+    htmlPdf.create(data.html, options).toStream(function (error, stream) {
+        if (error) {
+            result(error);
+        }
+        return stream.pipe(result.stream);
+    });
+}
